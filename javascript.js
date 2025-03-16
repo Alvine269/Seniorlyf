ff40c9b0578efe2c89eedaefabee90b12beb880f
Alvine269 <alvinetentacion999@gmail.com>
2025-03-16 22:34:14 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_qyac9fd","template_gyfbq6u,parms").then(alert("Email Sent!!"))
+}
